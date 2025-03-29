272fed3251b3a7baf90545a08845df458c40e5cb
john4650-hub <johndelvin51@gmail.com>
2025-03-29 21:22:01 +0300

    corrected paths to avoid cross origin errors

--- check_links.sh
+++ check_links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 local_files=("{{ url_for('static', filename='css/style.css')}}" "{{ url_for('static', filename='js/src/streams.js')}}" "/deps/bootstrap.min.css" "/deps/all.min.css" "/deps/bootstrap.min.js" "/deps/popper.min.js" "<script src=\"{{ url_for('static', filename='js/utils/utility.js')}}\"></script>" "/deps/all.min.js")
-remote_files=("https://raw.githubusercontent.com/StudyWindow/StudyWindow.github.io/refs/heads/main/css/style.css" "https://raw.githubusercontent.com/StudyWindow/StudyWindow.github.io/refs/heads/main/js/src/streams.js" "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/all.min.css" "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" "<!--nothing-->" "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/js/all.min.js")
+remote_files=("css/style.css" "js/src/streams.js" "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/all.min.css" "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" "<!--nothing-->" "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/js/all.min.js")
 
 pages=("streams" "about" "favorites" "leaderboard")
 for page in ${pages[@]}; do
@@ -24,6 +24,11 @@ for file in $files; do
         for index in $(seq 0 $((length_pages - 1)));do
           old="${local_files[$index]}"
           new="${remote_files[$index]}"
+           if printf "%s.html\n" "${pages[@]}" | grep -q -w "$name";then
+             if [ $(( $index <= 1 )) -eq 1 ];then
+               new="../$new"
+             fi
+        fi
           sed -i "s|$old|$new|g" $filename
         done
     fi
